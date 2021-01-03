390f81760e0e16d1267a84ef74fcac14488e07ef
Milahn <milahnmartin.develop@gmail.com>
2021-01-03 19:55:27 +0200

    new

--- database.js
+++ database.js
@@ -1,33 +1,12 @@
-var mongo = require('mongodb');
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://milahn:Benjie2302@fingerprint-cluster.dt8fn.mongodb.net/players?retryWrites=true&w=majority";
+  // Set the configuration for your app
+  // TODO: Replace with your project's config object
+  var config = {
+    apiKey: "apiKey",
+    authDomain: "projectId.firebaseapp.com",
+    databaseURL: "https://databaseName.firebaseio.com",
+    storageBucket: "bucket.appspot.com"
+  };
+  firebase.initializeApp(config);
 
-
-function insertNew(){
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("players");
-    var myobj =  {};
-  ;
-    dbo.collection("players").insertOne(myobj, function(err, res) {
-      if (err) throw err;
-      console.log("1 document inserted");
-      db.close();
-    });
-  });
-}
-
-function findP(){
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("players");
-    dbo.collection("players").findOne({"user":"KAREEM"}, function(err, result) {
-      if (err) throw err;
-      console.log(result.social_mediax1);
-      db.close();
-    });
-  });
-}
-
-
-findP();
+  // Get a reference to the database service
+  var database = firebase.database();
