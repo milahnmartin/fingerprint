6c6183f2dc46a7a030dcf8ddb38ec4409fa9bdec
Milahn <milahnmartin.develop@gmail.com>
2021-01-06 00:06:06 +0200

    Search working

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -26,6 +26,7 @@ const input = document.getElementById('user-q').value;
     
       if(typeof(snapshot.val()[input]) === 'object'){
         showData();
+        const data = snapshot.val()[input];
         
         var player_name = document.getElementById('player-name').innerHTML = "We found this for " + input + " !";
         var dym_text = document.getElementById('dym').innerHTML = '';
@@ -34,7 +35,39 @@ const input = document.getElementById('user-q').value;
           $('html, body').animate({
             scrollTop: $(".fa-desktop").offset().top +510
           }, 2000);
-       
+       document.getElementById('player-header-info').innerHTML = input + " Peripherals"
+       document.getElementById('player-basic-info').innerHTML = input + " basic Info"
+        document.getElementById('monitor-info').innerHTML = data.monitor;
+        document.getElementById('player-ping').innerHTML = data.ping;
+        document.getElementById('player-mouse').innerHTML = data.mouse;
+        document.getElementById('player-keyboard').innerHTML = data.keyboard;
+        document.getElementById('player-headset').innerHTML = data.headset;
+        if(data.controller === ''){
+          document.getElementById('player-controller').innerHTML = 'none';
+        }else{
+          document.getElementById('player-controller').innerHTML = data.controller;
+        }
+        // platform info
+        document.getElementById('player-platform-info').innerHTML = input + " Platform"
+        document.getElementById('player-basic').innerHTML = input + " Platform Info"
+        document.getElementById('player-ram').innerHTML = data.ram;
+        document.getElementById('player-motherboard').innerHTML = data.mobo;
+        document.getElementById('player-cpu').innerHTML = data.cpu;
+        document.getElementById('player-gpu').innerHTML = data.gpu;
+
+        // player team info
+        document.getElementById('player-team-info').innerHTML = input + " Team"
+        document.getElementById('team-basic').innerHTML = input + " Team Status and Contract Status"
+        console.log(data.signed)
+        console.log(typeof(data.signed))
+        if(data.signed === 'none'){
+          document.getElementById('player-team-status').innerHTML = 'Player Not Currently Signed';
+          document.getElementById('player-team').innerHTML = 'Free Agent';
+        }else{
+          document.getElementById('player-team-status').innerHTML = 'Player Currently Signed';
+          document.getElementById('player-team').innerHTML = data.signed;
+        }
+        
         
       }else{
         erroData();
