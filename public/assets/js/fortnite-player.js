3461b5b6290a81937cf0cd252510d07d855482c7
Milahn <milahnmartin.develop@gmail.com>
2021-01-14 22:42:12 +0200

    updaye

--- public/assets/js/fortnite-player.js
+++ public/assets/js/fortnite-player.js
@@ -13,6 +13,12 @@ rootRef.on('value', data => {
         let main_social = element.val().main_social;
         let ping = element.val().ping;
         let player_team_status;
+        let new_ping = " ";
+        let alp = ['a','b','c','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+        for(let i = 0 ;i < 3;i++){
+           new_ping += ping[i];
+            
+        }
 
         if (player_signed === 'None' || player_signed === 'none' || player_signed === "" || player_signed === 'Not signed'){
             player_team_status = 'F'
@@ -24,7 +30,7 @@ rootRef.on('value', data => {
     <tr>
     <th scope="row">${i}</th>
     <td>${user}</td>
-    <td>${ping}</td>
+    <td>${new_ping}</td>
     <td>${player_team_status}</td>
   </tr>
   `
