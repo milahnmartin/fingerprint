19e3b93b673831d4b90bfa090d76d1294a4859cc
milahnmartin <milahnmartin.develop@gmail.com>
2021-01-31 19:23:37 +0200

    initial update

--- public/ts to js/counterstrike.js
+++ public/ts to js/counterstrike.js
@@ -1,11 +1,20 @@
 const database = firebase.database();
 const rootref = database.ref('/users/CS');
-let myurl = new URL('https://fingerprint.com/counterstrike/');
+let myurl = new URL(window.location.href);
 console.log(myurl);
-myurl.searchParams.set('player', 'BNY');
 let myplayer = myurl.searchParams.get('player');
+myplayer = myplayer.toUpperCase();
 console.log(myplayer);
 rootref.on('value', info => {
-    let mydata = info.val()[myplayer];
-    console.log(mydata);
+    let mydata = new Object();
+    mydata = info.val();
+    let detailedplayer = new Object();
+    detailedplayer = mydata[myplayer];
+    if (detailedplayer == undefined) {
+        console.log('user doesnt exist');
+    }
+    else {
+        console.log(detailedplayer);
+        console.log(detailedplayer.monitor);
+    }
 });
--- public/ts/counterstrike.ts
+++ public/ts/counterstrike.ts
@@ -3,13 +3,13 @@ const database = firebase.database();
 const rootref = database.ref('/users/CS');
 
 
-let myurl = new URL('https://fingerprint.com/counterstrike/')
+let myurl = new URL(window.location.href)
 console.log(myurl);
 
-myurl.searchParams.set('player','BNY');
+
 
 let myplayer = myurl.searchParams.get('player');
-
+myplayer = myplayer.toUpperCase();
 console.log(myplayer);
 
 
@@ -17,9 +17,19 @@ console.log(myplayer);
 
 
 rootref.on('value', info => {
-    let mydata = info.val()[myplayer];
-
-    console.log(mydata);
+    let mydata = new Object();
+    mydata = info.val();
+    let detailedplayer = new Object();
+    detailedplayer =  mydata[myplayer];
+    if(detailedplayer == undefined){
+        console.log('user doesnt exist')
+    }else{
+        console.log(detailedplayer);
+        console.log(detailedplayer.monitor);
+    }
+    
+    
+    
 })
 
 
