a2e8d7032e357da3a3f0d1ee240684e57e4c2656
milahnmartin <milahnmartin.develop@gmail.com>
2021-02-11 18:57:54 +0200

    init cast buttons

--- public/ultrafy-js/index.js
+++ public/ultrafy-js/index.js
@@ -3,3 +3,28 @@ discordBTN.addEventListener('click', e => {
     e.preventDefault();
     window.open('https://discord.gg/eRmbrPw7Tu');
 });
+
+
+let reg_click = document.getElementById('reg-click');
+let cs_click = document.getElementById('cs-click');
+let cast_click = document.getElementById('cast-click');
+
+
+
+reg_click.addEventListener('click', e => {
+    e.preventDefault();
+
+    window.location.assign('./register-cs');
+});
+
+
+cs_click.addEventListener('click',e => {
+    e.preventDefault();
+    window.location.assign('./search-counterstrike')
+})
+
+
+cast_click.addEventListener('click', e => {
+    e.preventDefault();
+    window.location.assign('#');
+})
